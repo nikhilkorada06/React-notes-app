71c8c72c8747d77cc41a24be8c3ed25e8080fa7c
nikhilkorada06 <nikhilkorada06@gmail.com>
2025-12-23 00:09:02 +0530

    except local storage everything i s completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,100 @@
 import React from 'react'
-import notesLogo from './assets/notes-app-symbol.jpg'
+import {useState} from 'react'
+import notesLogo from './assets/notes-app-symbol.jpg';
+import { X } from 'lucide-react';
 
 const App = () => {
 
-  const submitHandler = (e) => {
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+  const [todo, setTodo] = useState([]);
+
+  const submitHandler = (e, title, description) => {
     e.preventDefault();
+    setTitle("");
+    setDescription("");
+
     console.log("Form Submitted");
+
+    setTodo([...todo, {title, description}]);
+  }
+
+  const deleteHandler = (index) => {
+    const newTodo = todo.filter((item, idx) => idx !== index);
+    setTodo(newTodo);
   }
 
   return (
-    <div>
-      <h1 className="text-7xl font-bold mb-6 text-center">Notes App</h1>
-      <form onSubmit={submitHandler} className='flex flex-row gap-30 mb-4 mt-10 justify-center px-10'>
-        <div className='flex flex-col gap-4 mb-4 w-[60%] ml-30'>
-          <input className='border border-black-600 rounded px-2 py-2 outline-none font-medium' type="text" placeholder="Enter your note Title..." />
-          <textarea rows="4" cols="70" className='border border-black-600 outline-none rounded px-2 py-2 font-medium' type="text" placeholder="Description..." />
-          <button type="submit" className='bg-black text-white px-4 py-2 rounded cursor-pointer'>Add Note</button>
+    <>
+      <div className='flex flex-row align-items-center justify-center mt-10'>
+        
+        <h1 className="text-5xl font-bold mb-6 flex align-items-center pt-4">Notes App</h1>
+        <img className='h-20 w-20 ml-3 rotate-y-180' src={notesLogo} alt="notes" />
+      </div>
+
+
+      <form 
+            onSubmit={(e) => {submitHandler(e, title, description)}} 
+            className='flex flex-row gap-30 mb-4 mt-10 justify-center px-10'
+      >
+
+        <div className='flex flex-col gap-4 mb-4 w-80 '>
+
+          <input 
+                className='border border-black-600 rounded px-2 py-2 outline-none font-bold' 
+                type="text" 
+                placeholder="Enter your note Title..." 
+                value={title}
+                onChange={(e) => setTitle(e.target.value)}
+          />
+          <textarea 
+                rows="4" 
+                cols="70" 
+                type="text" 
+                placeholder="Description..."
+                className='border border-black-600 outline-none rounded px-2 py-2 font-semibold'
+                value={description}
+                onChange={(e) => setDescription(e.target.value)}
+          />
+          <button 
+                type="submit" 
+                className='bg-black text-white active:bg-gray-100 px-4 py-2 rounded cursor-pointer'
+          >   
+                Add Note
+          </button>
+
         </div>
-        <img className='h-60 w-60 rotate-y-180 mr-30 mb-10 pb-10' src={notesLogo} alt="notes" />
       </form>
 
-      <div className='flex flex-wrap p-10'>
-        <div className='h-32 w-62 bg-blue-200 rounded-2xl '> </div>
+      <div className='flex flex-wrap p-10 gap-3 justify-center'>
+
+        {todo.map((item, idx) => (
+
+          <div 
+              key={idx} 
+              className='relative h-62 w-72 object-fit bg-[url(https://purepng.com/public/uploads/medium/purepng.com-yellow-sticky-notessticky-notesclipartyellowpinnedtaped-142152649982192nye.png)] bg-cover rounded-2xl p-1'
+          >
+
+              <button 
+                      onClick={() => deleteHandler(idx)} 
+                      className='active:scale-90 absolute top-2 right-9 bg-red-500 text-white rounded-full p-1 hover:bg-red-700 cursor-pointer'
+              >
+                <X size={16} />
+              </button>
+
+              <h2 
+                  className='text-xl font-bold mb-2 flex justify-center text-black pt-7 mr-5'
+              >
+                    {item.title} 
+              </h2>
+
+              <p className='text-md text-gray-600 pl-4 pt-2'>{item.description}</p>
+          </div>
+
+        ))}
+
       </div>
-    </div>
+    </>
   )
 }
 
